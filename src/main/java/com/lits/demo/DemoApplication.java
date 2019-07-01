84b21d32a87d49f5305efd865e23e897c530dbbe
Serhiy Perepelyuk <37741217@gmail.com>
2019-07-01 13:42:33 +0300

    update 01/07/2019. migration - ok Security problems - to be solved lombock connected. swagger connected, but I cannot login to check if it works.

--- src/main/java/com/lits/demo/DemoApplication.java
+++ src/main/java/com/lits/demo/DemoApplication.java
@@ -1,15 +1,28 @@
 package com.lits.demo;
 
+import com.lits.demo.model.UserEntity;
+import com.lits.demo.repository.UserDataRepository;
 import io.swagger.jaxrs.config.BeanConfig;
 import javax.ws.rs.core.Application;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @SpringBootApplication
-public class DemoApplication extends Application {
+public class DemoApplication extends Application implements ApplicationRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	UserDataRepository userDataRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -29,5 +42,12 @@ public class DemoApplication extends Application {
 		resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
 		return resources;
 	}
-
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		UserEntity user = new UserEntity();
+		user.setUsername("loco");
+//		user.setPassword("$2a$10$xIqXZRbKo96Y/h1uZa1eAuo53cNHIJffo5lqLsnLdhzfSpVB/XPoq");
+		user.setPassword(passwordEncoder.encode("123"));
+		userDataRepository.save(user);
+	}
 }
