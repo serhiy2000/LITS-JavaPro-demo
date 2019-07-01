b5e7c3ce34bf3ea0e71a62cb4e3e348a6aab9e35
Serhiy Perepelyuk <37741217@gmail.com>
2019-07-02 00:32:52 +0300

    update 01/07/2019. migration - ok Security problems - to be solved lombock connected. swagger connected, but I cannot login to check if it works.

--- src/main/java/com/lits/demo/config/SecurityConfig.java
+++ src/main/java/com/lits/demo/config/SecurityConfig.java
@@ -81,7 +81,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**/login").permitAll()
 
                 .anyRequest().authenticated();
-
+//
         httpSecurity
                 .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
 
